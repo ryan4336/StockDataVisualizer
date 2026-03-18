566898ebb9c911345db724a75db274a1469690fc
ryan4336 <rahhhp@umsystem.edu>
2026-03-18 11:23:18 -0500

    Initial file set up and vague outline

--- app.py
+++ app.py
@@ -1,1 +1,71 @@
 #import necessary modules
+
+
+
+
+API_KEY = "XAUMT7K9CR2S8LVB"
+"""
+API KEY LIMITS:
+Daily Limit: You can make a maximum of 25 API requests in a 24-hour period.
+Per-Minute Limit: You are limited to 5 API requests per minute.
+"""
+
+# ------------------------------
+# GET STOCK SYMBOL AND VERIFY
+# ------------------------------
+
+
+
+
+# ------------------------------
+# GET CHART TYPE AND VERIFY
+# ------------------------------
+
+
+
+
+
+# -------------------------------------
+# GET TIME SERIES FUNCTION  AND VERIFY
+# -------------------------------------
+
+
+
+
+
+# -------------------------------------
+# GET START AND END DATE AND VERIFY
+# -------------------------------------
+
+
+
+
+
+# -------------------------------------
+# FILTER DATA BY DATE RANGE
+# -------------------------------------
+
+
+
+
+
+
+# -------------------------------------
+# GENERATE CHART AND OPEN IN BROWSER
+# -------------------------------------
+
+
+
+
+
+# -----------------------------
+# MAIN PROGRAM
+# -----------------------------
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
